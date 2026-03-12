7c9b2fd9a28072f337efc32e3b56c11cd969a168
Juan Cerda <ju.cerdar@duocuc.cl>
2026-03-11 21:21:00 -0300

    First commit

---
+++ src/main/java/app/gestionviajes/GestionDeViajesApplication.java
@@ -0,0 +1,13 @@
+package app.gestionviajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeViajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeViajesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/gestionviajes/GestionDeViajesApplicationTests.java
@@ -0,0 +1,13 @@
+package app.gestionviajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeViajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
