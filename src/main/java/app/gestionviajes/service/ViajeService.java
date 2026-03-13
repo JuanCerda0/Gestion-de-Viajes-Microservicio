bb66d3066b80b63d7726f6260776716b96190dac
Juan Cerda <ju.cerdar@duocuc.cl>
2026-03-12 21:28:23 -0300

    Se termino de armar el servicio de Viaje

--- src/main/java/app/gestionviajes/service/ViajeService.java
+++ src/main/java/app/gestionviajes/service/ViajeService.java
@@ -25,4 +25,39 @@ public class ViajeService {
         return repository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Viaje no encontrado"));
     }
+
+    public List<Viaje> listarViajes() {
+        return repository.findAll();
+    }
+
+    public Viaje iniciarViaje(Long id) {
+        Viaje viaje = obtenerViaje(id);
+        viaje.setEstado("EN_CURSO");
+        
+        viaje.setFechaInicio(LocalDateTime.now());
+        
+        return repository.save(Viaje);
+    }
+    
+    public Viaje finalizarViaje(Long id, Double costoFinal, Integer tiempoReal) {
+        
+        Viaje viaje = obtenerViaje(id); 
+        Viaje.setEstado("FINALIZADO");
+        
+        viaje.setFechaFin(LocalDateTime.now());
+        viaje.setCostoFinal(costoFinal);
+        viaje.setTiempoReal(tiempoReal);
+        
+        return repository.save(viaje);
+    }
+
+    public Viaje cancelarViaje(Long id) {
+       
+        Viaje viaje = obtenerViaje(id);
+        viaje.setEstado("CANCELADO");
+        
+        return repository.save(viaje);
+    }
+    
+    
 }
